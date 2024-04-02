62321428de57a647f1473386531859757563d97d
insightfulfuture <futureinsightful@gmail.com>
2024-04-02 10:47:00 -0400

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
+  </React.StrictMode>
+);
